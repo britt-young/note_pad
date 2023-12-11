b3283f9edc441fb2c6c96972720805805558416c
Britt Young <140761880+britt-young@users.noreply.github.com>
2023-12-11 14:44:37 -0500

    refactor
    css linking page

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('../routes/apiRoutes');
-const htmlRoutes = require('../routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
 
